d32fbbaa6f9b6cf6c96611134cbb8ac49b49b301
Andy He <andy.he@duke.edu>
2024-02-02 12:45:41 -0500

    Completed Front End for C4

--- connect4/view.py
+++ connect4/view.py
@@ -1,84 +1,81 @@
 import arcade
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Starting Template"
+from connect4.board_c4 import BoardC4
+from connect4.oracle_c4 import OracleC4
+# from connect4.player_c4 import PlayerC4
+
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 700
+
+class C4Piece():
+    def __init__(self, r, c, color):
+        self.r = r
+        self.c = c
+        self.color = color
 
 
-class MyGame(arcade.Window):
-    """
-    Main application class.
+class C4Game(arcade.Window):
 
-    NOTE: Go ahead and delete the methods you don't need.
-    If you do need a method, delete the 'pass' and replace it
-    with your own code. Don't leave 'pass' in this program.
-    """
-
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title)
+    def __init__(self, width, height):
+        super().__init__(width, height, "Connect 4")
         arcade.set_background_color(arcade.color.WHITE)
+        self.pieces = []
+        self.selected = -1 # which column player selected
 
     def setup(self):
-        """Set up the game variables. Call to re-start the game."""
-        # Create your sprites and sprite lists here
-        pass
-
+        self.board = BoardC4.from_start()
+        self.result = 2
+        self.bot = OracleC4(depth=4)
+        # self.bot = PlayerC4()
+    
     def on_draw(self):
-        """
-        Render the screen.
-        """
-
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
         self.clear()
 
-        # Call draw() on all your sprite lists below
+        BORDER = 50
+        for i in range(1, 9):
+            x_position = i * SCREEN_WIDTH / 9
+            arcade.draw_line(x_position, BORDER, x_position, SCREEN_HEIGHT-BORDER, arcade.color.BLACK, 2)
+        arcade.draw_line(1 * SCREEN_WIDTH / 9, BORDER, 8 * SCREEN_WIDTH / 9, BORDER, arcade.color.BLACK,2)
+        arcade.draw_line(1 * SCREEN_WIDTH / 9, SCREEN_HEIGHT-BORDER, 8 * SCREEN_WIDTH / 9, SCREEN_HEIGHT-BORDER, arcade.color.BLACK,2)
+            
+        if self.board.red_move:
+            self.board = self.bot.move(self.board)
+            self.update_board(self.board)
+        elif self.selected != -1 and self.board.bottom_available(self.selected) != 7:
+            self.board = self.board.make_move((self.selected, self.board.bottom_available(self.selected)))
+            self.update_board(self.board)
+            self.selected = -1
+        
+        self.result = self.board.terminal_eval()
 
-    def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
-        pass
+        for piece in self.pieces:
+            arcade.draw_circle_filled(piece.r, piece.c, 50, piece.color)
 
-    def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
+        if self.result != 2:
+            text = "Red wins" if self.result == 1 else "Blue wins" 
+            text = "Draw" if self.result == 0 else text
+            arcade.draw_text(text, 450, 675, arcade.color.BLACK, 15, anchor_x="center", anchor_y="center")
+            
 
-        For a full list of keys, see:
-        https://api.arcade.academy/en/latest/arcade.key.html
-        """
-        pass
-
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
-
+    def update_board(self, board):
+        board = board.board_matrix
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                x_pos = 150 + i * 100
+                y_pos = 100 + j * 100
+                color = arcade.color.RED if board[i][j] == 1 else arcade.color.BLUE
+                if board[i][j] != 0:
+                    self.pieces.append(C4Piece(x_pos, y_pos, color))
+    
     def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
+        if self.result != 2:
+            arcade.Window.close(self)
+        if 100 < x < 800:
+            self.selected = ((x-100) // 100)
 
 
 def main():
-    """Main function"""
-    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game = C4Game(SCREEN_WIDTH, SCREEN_HEIGHT)
     game.setup()
     arcade.run()
 
