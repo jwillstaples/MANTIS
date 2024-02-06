a647c671f1d5b96c273754dae53c7da8150b957e
jwillstaples <jwillstaples@gmail.com>
2024-02-06 13:26:36 -0500

    wills changes'

--- connect4/human_bot_game.py
+++ connect4/human_bot_game.py
@@ -42,17 +42,19 @@ if __name__ == "__main__":
     game_board = BoardC4.from_start()
     result = 2
 
-    # bot = PlayerC4()
-    bot = OracleC4(depth=4)
+    
+    bot1 = OracleC4(depth=3)
+    bot2 = OracleC4(depth=4)
 
     print_board(game_board)
 
     while result == 2:
         if game_board.red_move:
-            game_board = bot.move(game_board)
+            game_board = bot1.move(game_board)
 
         else:
-            game_board = take_human(game_board)
+            # game_board = take_human(game_board)
+            game_board = bot2.move(game_board)
 
         print_board(game_board)
         result = game_board.terminal_eval()
