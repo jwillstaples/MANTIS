3f79f710ee10e13590d59f004d6219911f7a6891
Andy He <xia.yifu82@gmail.com>
2024-02-26 21:04:19 +0000

    spawn instead of fork, added new functions to allow pickling of nets

---
+++ connect4/.ipynb_checkpoints/board_c4-checkpoint.py
@@ -0,0 +1,142 @@
+from typing import List, Tuple
+import numpy as np
+import torch
+
+from common.board import BlankBoard
+from scipy.signal import convolve2d
+
+
+class BoardC4(BlankBoard):
+    def __init__(self, board_matrix: np.ndarray, red_move: bool):
+        """
+        board_matrix: 6x7 numpy array holding integer values. 1: red, 0: empty, -1: yellow
+        red_move: boolean holding whether it's red's move. We assume red moves first.
+        """
+
+        self.board_matrix = board_matrix
+        self.red_move = red_move
+
+    def get_legal_moves(self) -> List["BoardC4"]:
+        """return List of boards representing legal moves"""
+
+        legal_placements = []
+        for col in range(7):
+            row = self.bottom_available(col)
+            if row != 7:
+                legal_placements.append(self.make_move((col, row)))
+
+        return legal_placements
+
+    def legal_moves(self) -> np.ndarray:
+        """boolean array of which moves are legal"""
+        return np.sum(np.abs(self.board_matrix), axis=1) < 5.5
+
+    def open_cols(self) -> List[int]:
+        """Returns columns with an open space"""
+
+        open_cols = []
+        for i, col in enumerate(self.board_matrix):
+            if np.sum(np.abs(col)) < 7:
+                open_cols.append(i)
+
+        return open_cols
+
+    def bottom_available(self, col: int) -> int:
+        """returns bottom available slot in a given column"""
+
+        for i, val in enumerate(self.board_matrix[col]):
+            if val == 0:
+                return i
+
+        return 7
+
+    def terminal_eval(self) -> int:
+        horizontal_kernel = np.array([[1, 1, 1, 1]])
+        vertical_kernel = np.transpose(horizontal_kernel)
+        diag1_kernel = np.eye(4, dtype=np.uint8)
+        diag2_kernel = np.fliplr(diag1_kernel)
+        detection_kernels = [
+            horizontal_kernel,
+            vertical_kernel,
+            diag1_kernel,
+            diag2_kernel,
+        ]
+
+        for kernel in detection_kernels:
+            convolution = convolve2d(self.board_matrix, kernel, mode="valid")
+            if (convolution == 4).any():
+                return 1
+            if (convolution == -4).any():
+                return -1
+
+        if not (self.board_matrix == 0).any():
+            return 0
+
+        return 2
+
+    def make_move(self, move):
+        new_board = self.board_matrix.copy()
+        new_board[move[0]][move[1]] = 1 if self.red_move else -1
+
+        return BoardC4(new_board, not self.red_move)
+
+    def move_from_int(self, col: int) -> "BoardC4":
+        # assert col in self.open_cols()
+        assert self.legal_moves()[col]
+        move = (col, self.bottom_available(col))
+        return self.make_move(move)
+
+    @classmethod
+    def from_start(cls):
+        return cls(np.zeros((7, 6), dtype=int), True)
+
+    def to_tensor(self) -> torch.tensor:
+        tb = torch.from_numpy(self.board_matrix)
+        t1 = (tb == 1).float()
+        t0 = (tb == 0).float()
+        tn1 = (tb == -1).float()
+        if self.red_move:
+            return torch.stack([t1, t0, tn1])
+        return torch.stack([tn1, t0, t1])
+
+    def player_perspective_eval(self) -> int:
+        terminal = self.terminal_eval()
+
+        if terminal != 2:
+            mult = 1 if self.red_move else -1
+            return mult * terminal
+        return terminal
+
+    def __str__(self):
+        for row in self.board_matrix.T[::-1]:
+            print("|", end="")
+            for val in row:
+                if val == 1:
+                    print("x", end="")
+                elif val == -1:
+                    print("o", end="")
+                else:
+                    print(" ", end="")
+                print("|", end="")
+            print("")
+        for i in range(7):
+            print(f" {i}", end="")
+        print("\n")
+        return ""
+
+
+def print_board(board: BoardC4) -> None:
+    for row in board.board_matrix.T[::-1]:
+        print("|", end="")
+        for val in row:
+            if val == 1:
+                print("x", end="")
+            elif val == -1:
+                print("o", end="")
+            else:
+                print(" ", end="")
+            print("|", end="")
+        print("")
+    for i in range(7):
+        print(f" {i}", end="")
+    print("\n")
---
+++ connect4/.ipynb_checkpoints/c4net-checkpoint.py
@@ -0,0 +1,101 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+import time
+
+
+class C4Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.startBlock = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1), nn.BatchNorm2d(64), nn.ReLU()
+        )
+
+        self.backBone = nn.ModuleList([ResBlock(64) for i in range(2)])
+
+        self.policyHead = nn.Sequential(
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(32 * 6 * 7, 7),
+        )
+
+        self.valueHead = nn.Sequential(
+            nn.Conv2d(64, 3, kernel_size=3, padding=1),
+            nn.BatchNorm2d(3),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(3 * 42, 1),
+            nn.Tanh(),
+        )
+
+        self.apply(self.init_weights)
+
+    def forward(self, x):
+        x = self.startBlock(x)
+        for resBlock in self.backBone:
+            x = resBlock(x)
+        policy = self.policyHead(x)
+        value = self.valueHead(x)
+        return F.softmax(policy, dim=1), value
+
+    def init_weights(self, m):
+        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+            init.normal_(m.weight, mean=0, std=0.02)
+            if m.bias is not None:
+                init.zeros_(m.bias)
+
+
+class ResBlock(nn.Module):
+    def __init__(self, num_hidden):
+        super().__init__()
+        self.conv1 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_hidden)
+        self.conv2 = nn.Conv2d(num_hidden, num_hidden, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(num_hidden)
+
+    def forward(self, x):
+        residual = x
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = self.bn2(self.conv2(x))
+        x += residual
+        x = F.relu(x)
+        return x
+
+
+def test():
+    N, ch, R, C = 10, 3, 6, 7
+    rand = torch.randn((N, ch, R, C))
+    net = C4Net()
+    p, v = net(rand)
+    assert p.shape == torch.Size([N, 7])
+    assert v.shape == torch.Size([N, 1])
+    print("Works!")
+
+
+def test2():
+    times = 200
+
+    st = time.time()
+    for _ in range(times):
+        N, ch, R, C = 5000, 3, 6, 7
+        rand = torch.randn((N, ch, R, C))
+        net = C4Net()
+        p, v = net(rand)
+    print(f"time: {time.time() - st}")
+
+    st = time.time()
+    times *= 10
+    for _ in times:
+        N, ch, R, C = 1, 3, 6, 7
+        rand = torch.randn((N, ch, R, C))
+        net = C4Net()
+        p, v = net(rand)
+    print(f"time: {time.time() - st}")
+
+# test()
+
+# test2()
