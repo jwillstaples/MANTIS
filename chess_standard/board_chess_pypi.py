719c9ea44e1b2f5013201bc7c27131a17d5c7b5b
AshritVerma <71358894+AshritVerma@users.noreply.github.com>
2024-03-26 00:52:40 -0400

    Update board_chess_pypi.py

--- chess_standard/board_chess_pypi.py
+++ chess_standard/board_chess_pypi.py
@@ -240,8 +240,8 @@ UCI_ARRAY = ["a1b1",  "a1c1",  "a1d1",  "a1e1",  "a1f1",  "a1g1",  "a1h1",  "a1a
     "c7b8n", "c7d8n", "d7c8n", "d7d8n", "d7e8n", "e7d8n", "e7e8n", "e7f8n",
     "f7e8n", "f7f8n", "f7g8n", "g7f8n", "g7g8n", "g7h8n", "h7g8n", "h7h8n", 
     "h2g1q", "b2c1q", 'e2f1q', 'e2f1n', 'e2f1b', 'e2f1r', 'a2b1q', 'b2c1r', 
-    'c2d1q', 'f2f1q', 'b2c1b', 'b2c1n','d2c1q', 'd2c1r', 'd2c1n', 'd2c1b',  'g2h1r',
-    'g2h1n', 'g2h1b', 'g2h1q', 'b2b1q', 'b2b1n', 'b2b1r', 'b2b1r']
+    'c2d1q', 'c2d1n', 'c2d1r', 'c2d1b', 'f2f1q', 'b2c1b', 'b2c1n','d2c1q', 'd2c1r', 'd2c1n', 'd2c1b',  'g2h1r',
+    'g2h1n', 'g2h1b', 'g2h1q', 'b2b1q', 'b2b1n', 'b2b1r', 'b2b1r',  'b2b1b']
              
 OUTPUT_LENGTH = len(UCI_ARRAY)
 
