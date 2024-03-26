631059f35ae687afc791ba79b30bddf4ff0b64c4
AshritVerma <71358894+AshritVerma@users.noreply.github.com>
2024-03-26 00:45:46 -0400

    Update board_chess_pypi.py

--- chess_standard/board_chess_pypi.py
+++ chess_standard/board_chess_pypi.py
@@ -239,7 +239,8 @@ UCI_ARRAY = ["a1b1",  "a1c1",  "a1d1",  "a1e1",  "a1f1",  "a1g1",  "a1h1",  "a1a
     "h7h8r", "h7h8b", "a7a8n", "a7b8n", "b7a8n", "b7b8n", "b7c8n", "c7c8n",
     "c7b8n", "c7d8n", "d7c8n", "d7d8n", "d7e8n", "e7d8n", "e7e8n", "e7f8n",
     "f7e8n", "f7f8n", "f7g8n", "g7f8n", "g7g8n", "g7h8n", "h7g8n", "h7h8n", 
-    "h2g1q", "b2c1q", 'e2f1q', 'a2b1q', 'b2c1r', 'c2d1q', 'f2f1q']
+    "h2g1q", "b2c1q", 'e2f1q', 'e2f1n', 'e2f1b', 'e2f1r', 'a2b1q', 'b2c1r', 
+    'c2d1q', 'f2f1q', 'b2c1b']
 
 OUTPUT_LENGTH = len(UCI_ARRAY)
 
